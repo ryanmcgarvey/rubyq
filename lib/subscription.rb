6a8edf2a252605dcb16dab342e7323dd5f3c4ac3
Ryan McGarvey <mcgarvey.ryan@gmail.com>
2022-11-16 11:31:05 -0500

    Keep track of empty sockets

--- lib/subscription.rb
+++ lib/subscription.rb
@@ -8,12 +8,13 @@ class Subscription
 end
 
 class Job
-  attr_accessor(:event_id, :status, :time)
+  attr_accessor(:event_id, :status, :time, :ws)
 
-  def initialize(event_id, status, time)
+  def initialize(event_id, status, time, ws)
     @event_id = event_id
     @status = status
     @time = time
+    @ws = ws
   end
 
   def needs_retry
@@ -36,6 +37,7 @@ class SubscriptionManager
     @event_log = event_log
     @subscriptions = {}
     @jobs = {}
+    @sockets = {}
   end
 
   def add_subscription(ws, data)
@@ -50,17 +52,35 @@ class SubscriptionManager
             update_subscriptions(event) if (channel == event.channel)
           end
         end
+        add_socket(ws, channel)
       else
+        add_socket(ws, channel)
+        swap_sockets(subscriptions[channel].ws, ws, channel)
         subscriptions[channel] = Subscription.new(ws, channels)
+        cleanup_sockets
       end
     end
+  end
+
+  def add_socket(ws, channel)
+    if (@sockets[ws] == nil)
+      @sockets[ws] = [channel]
+    else
+      @sockets[ws].push(channel)
+    end
+  end
+
+  def swap_sockets(oldws, newws, channel)
+    @sockets[oldws].delete(channel)
+    @sockets[newws].push(channel)
   end
 
   def update_subscriptions(event)
     subscription = subscriptions[event.channel]
     if subscription
-      subscription.ws.send(event.to_json)
-      jobs[event.id] = Job.new(event.id, "sent", Time.now)
+      ws = subscription.ws
+      ws.send(event.to_json)
+      jobs[event.id] = Job.new(event.id, "sent", Time.now, ws)
       process_timeout(event.id)
     end
   end
@@ -69,19 +89,39 @@ class SubscriptionManager
     Thread.new do
       sleep(5)
       if (jobs[event_id].status == "sent")
-        jobs[event_id].status = "timeout"
+        job = jobs[event_id]
+        job.status = "timeout"
+        job.ws = nil
         p("timeout #{event_id}")
       end
+      cleanup_sockets
     end
   end
 
   def job_succeeded(event_id)
-    jobs[event_id].status = "success"
+    job = jobs[event_id]
+    job.status = "success"
+    job.ws = nil
+    cleanup_sockets
     p("success #{event_id}")
   end
 
   def job_failed(event_id)
-    jobs[event_id].status = "error"
+    job = jobs[event_id]
+    job.status = "error"
+    job.ws = nil
+    cleanup_sockets
     p("error #{event_id}")
+  end
+
+  def cleanup_sockets
+    @sockets.each do |ws, channels|
+      if (channels.length == 0)
+        if (jobs.values.select { |job| (job.status == "sent") and (job.ws == ws) }.length == 0)
+          ws.close
+          @sockets.delete(ws)
+        end
+      end
+    end
   end
 end
